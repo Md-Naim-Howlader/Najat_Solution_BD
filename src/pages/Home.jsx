be39210ef7bda1393194d8fb793cb10eb0de4146
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-10-22 11:20:12 +0600

    added our-company section and vercel page refresh problem solve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -159,9 +159,9 @@ const Home = () => {
                 alt="application features images"
               />
             </div>
-            <div className="col-lg-6 mt-5 mt-lg-0">
+            <div className="col-lg-6  mt-5 mt-lg-0">
               <div className="section-title">
-                <span className="after">MORE ABOUT OUR COMPANY</span>
+                <span className="before">MORE ABOUT OUR COMPANY</span>
                 <h2>Our Application Features.</h2>
                 <p>
                   IT service providers work closely with clients work clients to
@@ -259,6 +259,54 @@ const Home = () => {
             </div>
             <div className="col-lg-4">
               <img src="images/team-member.png" alt="team member" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="our-company section-pt">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7">
+              <div className="section-title">
+                <span className="before">WELCOME TO OUR COMPANY</span>
+                <h2>We provide Excellent IT services for your success</h2>
+              </div>
+              <div className="mt-5">
+                <div>
+                  <div className="progress-info d-flex justify-content-between">
+                    <span>BUSINESS SECURITY</span>
+                    <span>62%</span>
+                  </div>
+                  <div className="progres-bar ">
+                    <div className="progres progress1"></div>
+                  </div>
+                </div>
+                <div className="progress-item">
+                  <div className="progress-info d-flex justify-content-between">
+                    <span>CAREER DEVELOPMENT</span>
+                    <span>88%</span>
+                  </div>
+                  <div className="progres-bar ">
+                    <div className="progres progress2"></div>
+                  </div>
+                </div>
+                <div className="progress-item">
+                  <div className="progress-info d-flex justify-content-between">
+                    <span>BUSINESS INOVATION</span>
+                    <span>90%</span>
+                  </div>
+                  <div className="progres-bar ">
+                    <div className="progres progress3"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-5 mt-5 mt-lg-0">
+              <img
+                className="h-100 w-100"
+                src="images/our-company.png"
+                alt="our company image"
+              />
             </div>
           </div>
         </div>
