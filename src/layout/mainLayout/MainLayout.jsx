461054a28923b841fb283ce9228859508b02636d
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-10-28 14:41:24 +0600

    added ScrollTop component for routing top

--- src/layout/mainLayout/MainLayout.jsx
+++ src/layout/mainLayout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import ScrollTop from "../../components/ScrollTop";
 
 const MainLayout = () => {
   return (
     <>
       <Header />
       <main>
+        <ScrollTop />
         <Outlet />
       </main>
       <Footer />
