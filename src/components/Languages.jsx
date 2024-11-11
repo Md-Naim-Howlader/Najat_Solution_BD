11386f0bee7d3e2c7deeb2319ad7cdfacb580388
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-11-11 13:36:14 +0600

    added style for languages and countries

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,24 +1,55 @@
 import { useState } from "react";
-import useLanguages from "../hooks/useLanguages";
-import Select from "react-select"; // Correct path
+import useFetch from "../hooks/useFetch";
+import Select from "react-select";
 
 const Languages = () => {
-  const { languages, langLoading } = useLanguages(
-    "https://libretranslate.com/languages"
-  );
+  const { data, loading } = useFetch("https://libretranslate.com/languages");
+  // Sort languages alphabetically by name
+  const sortedLang = data.sort((a, b) => a.name.localeCompare(b.name));
+
+  const languageOptions = sortedLang.map((lang) => ({
+    value: lang.code,
+    label: lang.name,
+  }));
   const [selectedLanguage, setSelectedLanguage] = useState({
     value: "en",
     label: "English",
   });
+  const customStyles = {
+    control: (base) => ({
+      ...base,
+      border: 0,
+      boxShadow: "none",
+      padding: 0,
+      borderRight: "none",
+      "&:hover": {
+        border: 0,
+      },
+    }),
+    dropdownIndicator: (base) => ({
+      ...base,
+      border: "none",
+    }),
+    indicatorSeparator: () => ({
+      display: "none",
+    }),
+    valueContainer: (base) => ({
+      ...base,
+    }),
+    input: (base) => ({
+      ...base,
+    }),
+  };
 
   return (
-    <div className="ms-3">
-      {langLoading && <img className="loader" src={"images/loader.gif"} />}
+    <div className=" z-3 position-relative">
+      {loading && <img className="loader" src={"images/loader.gif"} />}
 
       <Select
         value={selectedLanguage}
         onChange={setSelectedLanguage}
-        options={languages}
+        options={languageOptions}
+        styles={customStyles}
       />
     </div>
   );
